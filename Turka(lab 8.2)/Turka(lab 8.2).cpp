e85747b1a74f4240632a41c76efe34130806f28a
Jambi <iturkas42@gmail.com>
2024-03-18 02:51:26 +0200

    Друге завдання.

--- Turka(lab 8.2)/Turka(lab 8.2).cpp
+++ Turka(lab 8.2)/Turka(lab 8.2).cpp
@@ -4,66 +4,45 @@ using namespace std;
 
 int main() {
 
-    setlocale(LC_CTYPE, "ukr");
-   
-    const int n = 6; 
-    double arr[n] = { 2.5, 0, -3, 4, -2, 6 }; 
+    setlocale(LC_CTYPE, "ukr"); 
 
-    // а)
-    double prod_non_zero = 1.0; 
+    const int n = 10; 
+    double arr[n] = {12, 3, 7, 9, 1, 0, 10, 32, 4, 22};  
 
-    for (int i = 0; i < n; ++i) {
+    bool has_duplicates = false; 
 
-        if (arr[i] != 0) {
+    for (int i = 0; i < n - 1; ++i) {
 
-            prod_non_zero *= arr[i]; 
-        }
-    }
-    cout << "Добуток елементiв, що не дорiвнюють нулю: " << prod_non_zero << '\n';
-    cout << " " << '\n';
+        for (int j = i + 1; j < n; ++j) {
 
-    // б)
-    double prod_between_negatives = 1.0; 
+            if (arr[i] == arr[j]) {
 
-    int firstNegativeIndex = -1;
-    int secondNegativeIndex = -1;
-
-    for (int i = 0; i < n; ++i) {
-
-        if (arr[i] < 0) {
-
-            if (firstNegativeIndex == -1) {
-
-                firstNegativeIndex = i;
-
-            }
-
-            else {
-
-                secondNegativeIndex = i;
+                has_duplicates = true; 
 
                 break;
+
             }
+        }
+
+        if (has_duplicates) {
+
+            break; 
+
         }
     }
 
-    if (firstNegativeIndex != -1 && secondNegativeIndex != -1) {
+    if (has_duplicates) {
 
-        for (int i = firstNegativeIndex + 1; i < secondNegativeIndex; ++i) {
-
-            prod_between_negatives *= arr[i];
-
-        }
+        cout << "У масиві є однаковi числа." << '\n';
     }
 
     else {
 
-        prod_between_negatives = 0.0; 
+        cout << "У масивi немає однакових чисел." << '\n';
 
     }
-
-    cout << "Добуток елементiв масиву, розташованих мiж першим i другим вiд’ємними елементами: " << prod_between_negatives << '\n';
 
     return 0;
 
 }
+
